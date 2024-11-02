f78cfdcd7e7e89eacc44e7f734dcba8cb3c56863
Sairam Chundru <sairam.chundru1432@gmail.com>
2024-11-02 22:56:19 +0530

    cart Item screen

---
+++ src/CartItems.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const CartItems = () => {
+  return (
+    <View>
+      <Text>CartItems</Text>
+    </View>
+  );
+};
+
+export default CartItems;
+
+const styles = StyleSheet.create({});
