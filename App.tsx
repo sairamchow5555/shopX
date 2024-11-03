8c9c077f13776a51c3801c747f859be677f76e6f
Sairam Chundru <sairam.chundru1432@gmail.com>
2024-11-03 15:49:17 +0530

    added CartProvider to wrap enter app

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -6,35 +5,38 @@ import LoginScreen from './src/LoginScreen';
 import HomeScreen from './src/HomeScreen';
 import CartItemsScreen from './src/CartItemsScreen';
 import ItemDetailsScreen from './src/ItemDetailsScreen';
+import {CartProvider} from './src/CartProvider';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="login"
-          component={LoginScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="itemdetails"
-          component={ItemDetailsScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="cartItems"
-          component={CartItemsScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="login"
+            component={LoginScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="home"
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="itemdetails"
+            component={ItemDetailsScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="cartItems"
+            component={CartItemsScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 };
 
